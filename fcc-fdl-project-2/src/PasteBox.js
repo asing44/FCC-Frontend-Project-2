fb015878475a8dce68579be0e3c7b667930c5766
asing44 <adamwarsing@gmail.com>
2024-01-18 12:30:32 -0500

    UI Created paste box

--- fcc-fdl-project-2/src/PasteBox.js
+++ fcc-fdl-project-2/src/PasteBox.js
@@ -1,8 +1,16 @@
-import './main.css';
+import './pasteBox.css';
 
 function PasteBox() {
   return (
-    <div className="PasteBox">
+    <div className="container">
+      <header className='container-header-w'>
+        <div className="header-left">
+          <i className="container-icon-action fa-regular fa-paste"></i>
+          <h2 className='container-heading'>Editor</h2>
+        </div>
+        <i className="container-icon-expand fa-solid fa-expand"></i>
+      </header>
+      <input className="paste-box" type="textbox"></input>
     </div>
   );
 }
